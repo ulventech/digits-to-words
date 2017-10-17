bf5324f9913e50d0e055339b6c9d99633f7aa61b
Daniel Dyrnes <thedanielforum@gmail.com>
2017-10-17 18:02:08 +0900

    Added more tests and updated README

--- digits_to_words_test.go
+++ digits_to_words_test.go
@@ -3,5 +3,12 @@ package digits_to_words
 import "testing"
 
 func TestDigitsToWords(t *testing.T) {
-		println(DigitsToWords(1111))
+	t1 := DigitsToWords(1111)
+	if t1 != "one thousand one hundred and eleventh" {
+		t.Errorf("Return was incorrect, got: %s, want: one thousand one hundred and eleventh", t1)
+	}
+	t2 := DigitsToWords(1)
+	if t2 != "first" {
+		t.Errorf("Return was incorrect, got: %s, want: first", t2)
+	}
 }
